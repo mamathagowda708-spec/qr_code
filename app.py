b8b6a1e53413b443c1ecfe878d78d693829b634b
mamathagowda708-spec <mamathagowda708@gmail.com>
2026-05-06 13:53:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ data=st.text_input("enter url")
 if st.button("Generate QR"):
    if data:
        qrqrcode.make(data)
-    qr.save("qr.png")
-    img = Image.open("qr.png")
-    with open("qr.png", "rb") as f:
-    st.download_button("Download QR", f, file_name="qr.png")
+       qr.save("qr.png")
+       img = Image.open("qr.png")
+       with open("qr.png", "rb") as f:
+         st.download_button("Download QR", f, file_name="qr.png")
 else:
   st.warning("Please enter some text")
